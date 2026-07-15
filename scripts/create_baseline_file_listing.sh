a4bb7941e796e8493317f192ba8b6668f432e773
Ekrem Seren <ekrem.seren@gmail.com>
2026-07-15 15:55:19 +0300

    chore(rebrand): repoint Makefile + file-listing configs from proto/cmp to proto/ttm

--- scripts/create_baseline_file_listing.sh
+++ scripts/create_baseline_file_listing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Simple helper script to just create a file which contains a listing of all the cmp proto files in the c4t branch
+# Simple helper script to just create a file which contains a listing of all the ttm proto files in the c4t branch
 # This is used by the dependency checker to determine whether a new version of a proto file can be reused
 # when it's not present in the c4t branch
 
